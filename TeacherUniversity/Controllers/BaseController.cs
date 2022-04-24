fecffa0aa6340004babbded9d6e26dfabe2c0786
Petar Petrov <ptpetrov99@gmail.com>
2022-04-24 11:02:59 +0300

    BaseController Introduced and Migrations folder moved one level up

---
+++ TeacherUniversity/Controllers/BaseController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TeacherUniversity.Controllers
+{
+    [Authorize]
+    public class BaseController : Controller
+    {
+    }
+}
