a7c0be12723cf1336703fa2f2ca738fcec5275dc
Petar Petrov <ptpetrov99@gmail.com>
2022-04-24 20:58:34 +0300

    Candidature Model Added

---
+++ TeacherUniversity.Infrastructure/Data/Candidature.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TeacherUniversity.Infrastructure.Data.Common.Enums;
+
+namespace TeacherUniversity.Infrastructure.Data
+{
+    public class Candidature
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        [Required]
+        public QualificationalDegree QualificationalDegree { get; set; }
+
+        [Required]
+        public Subject Subject { get; set; }
+
+        [Required]
+        public Status Status { get; set; }
+    }
+}
