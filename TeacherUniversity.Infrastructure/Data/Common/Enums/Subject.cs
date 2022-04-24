a0699f685a8456b0397c61b38502f4ff7ba4e1d0
Petar Petrov <ptpetrov99@gmail.com>
2022-04-24 20:47:13 +0300

    Ennumerations for Qualificational Degree, Status and Subject Added

---
+++ TeacherUniversity.Infrastructure/Data/Common/Enums/Subject.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeacherUniversity.Infrastructure.Data.Common.Enums
+{
+    public enum Subject
+    {
+        English,
+        Mathematics,
+        Litreture,
+        Arts,
+        Geography,
+        History,
+        Physics,
+        Chemistry,
+        Biology
+    }
+}
